12e223dd446251f7d1407721af3e4d060b2d5f0c
Angel Anton <hello@monday8am.com>
2026-08-27 18:14:18 +0200

    Updated apply false for google service plugin

--- app/explorer/build.gradle.kts
+++ app/explorer/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.google.services)
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics)
 }
 
